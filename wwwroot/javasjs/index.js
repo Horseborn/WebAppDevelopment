5ced78b2d938f92d90e7bfbcfd2ec9025dd988c0
Sebastian Krøger Holmen <79469928+Horseborn@users.noreply.github.com>
2022-01-05 16:33:12 +0100

    Add project files.

---
+++ wwwroot/javasjs/index.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+    var x = 0;
+    var s = "";
+
+    console.log("hello mates");
+
+    var theForm = $("#theForm");
+    theForm.hide();
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying a bloody cock and ball item")
+    })
+    
+    let productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.fadeToggle(500);
+    })
+
+});
